dd0370afa9ad2d9e81ebe628ceb843d6264697b3
samohtdor <70899878+samohtdor@users.noreply.github.com>
2021-12-20 18:39:35 -0500

    Create 1_Select_Basics.sql

---
+++ 1_Select_Basics.sql
@@ -0,0 +1,13 @@
+
+
+# Query the [Movies] table with columns [Id	Title	Director	Year	Length_minutes]
+
+SELECT title FROM movies;
+
+SELECT director FROM movies;
+
+SELECT title, director FROM movies;
+
+SELECT title, year FROM movies;
+
+SELECT * FROM movies;
